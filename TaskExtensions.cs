2f95b53cbb93ec9adcfdcb01c25f5d033bb55e44
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:22:02 -0700

    Add files via upload

---
+++ TaskExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace Penguin.Extensions.Async
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public static class TaskExtensions
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    {
+        #region Methods
+
+        /// <summary>
+        /// Waits for the task to complete and then returns the result
+        /// </summary>
+        /// <typeparam name="T">The result type</typeparam>
+        /// <param name="task">the task to await</param>
+        /// <returns>The result</returns>
+        public static T GetResult<T>(this Task<T> task)
+        {
+            task.Wait();
+
+            return task.Result;
+        }
+
+        #endregion Methods
+    }
+}
